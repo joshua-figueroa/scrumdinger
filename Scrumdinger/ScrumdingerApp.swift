e70ba6636726e1c44d13ddbd554294e0bc2b2683
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2023-08-05 22:38:07 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Joshua Figueroa on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
